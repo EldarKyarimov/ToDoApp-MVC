af1dcc0f87cec1fba7f73125c0a232a3a1bec69c
Eldar <eldar.kern@gmail.com>
2023-10-16 16:01:12 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import Model from './model.js';
+import View from './view.js';
+import Controller from './controller.js';
+
+function initialize() {
+    const model = new Model();
+    const view = new View();
+    const controller = new Controller(view, model);
+}
---
+++ view.js
@@ -0,0 +1,7 @@
+export default class View {
+    constructor() {
+        this.input = document.querySelector('#myInput');
+        this.addBtn = document.querySelector('#addBtn');
+        this.list = document.querySelector('#myUl');
+    }
+}
