9f898027c655e126a1774f8f23209ac016973572
Eldar <eldar.kern@gmail.com>
2023-10-27 19:29:00 +0400

    version 1.1

--- view.js
+++ view.js
@@ -14,29 +14,31 @@ export default class View {
 
             this.input.value = '';
         })
+
+        // this.input.addEventListener('keypress', (e) => {
+        //     if (e.key == 'Enter')
+        //         callback(this.input.value);
+
+        //     this.input.value = '';
+        // })
     }
 
-    // basic view Task's
     drawTodo(todo, callback, changeDisplay) {
         const li = document.createElement('li');
         li.id = todo.id;
         li.innerText = todo.value;
 
         this.ul.appendChild(li);
-        // ===============================
 
         li.addEventListener('dblclick', function () {
             li.classList = 'checked';
         })
 
-
-        // ===============================
-
         const dltButton = document.createElement('button');
         dltButton.innerHTML = 'X';
         dltButton.className = 'dltBtn';
 
-        // display change option ===============================
+        // display change option 
         this.upBtn.addEventListener('click', () => {
             changeDisplay(this.upBtn.id);
         });
@@ -44,7 +46,6 @@ export default class View {
         this.downBtn.addEventListener('click', () => {
             changeDisplay(this.downBtn.id);
         });
-        // =====================================================
 
         dltButton.addEventListener('click', () => {
             callback(dltButton.parentElement.id);
@@ -71,7 +72,5 @@ export default class View {
             } else this.drawTodo(todo, callback, changeDisplay);
         })
     }
-
-
 
 }
